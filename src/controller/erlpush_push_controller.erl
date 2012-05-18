63deeb346a3fad873da07bc4e44509be70057315
Philippe Jayet <philippe@easybox.ch>
2012-05-18 22:36:11 +0200

    work in progress

--- src/controller/erlpush_push_controller.erl
+++ src/controller/erlpush_push_controller.erl
@@ -2,8 +2,20 @@
 -compile(export_all).
 
 broadcast('GET', [AppId], ExtraInfo) ->
-	ok;
+    {user_id, UserId} = ExtraInfo,
+    case boss_db:find(AppId) of
+        Application ->
+            case Application:push_user_id() of
+                UserId -> {ok, [{user_id, UserId}, {application, Application}]};
+                _ -> not_found
+            end;
+        {error, Reason} ->
+            not_found
+    end;
 broadcast('POST', [AppId], ExtraInfo) ->
-	%% TODO
-	boss_flash:add(SessionID, notice, "Message sent", ""),
-	{redirect, [{action, "broadcast", app_id, "asdf"}]}.
+    %% TODO
+    boss_flash:add(SessionID, notice, "Message sent", ""),
+    {redirect, [{action, "broadcast"}, {app_id, AppId}]}.
+
+before_(_ActionName) ->
+    user_utils:require_login(SessionID).
